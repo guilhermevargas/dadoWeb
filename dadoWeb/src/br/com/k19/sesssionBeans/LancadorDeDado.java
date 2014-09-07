8c45433a2b9fd43a1844ae1263c01411733f840a
Guilherme Alves Vargas <guilherme.alves.vargas@outlook.com>
2014-09-07 19:50:42 -0300

    Implementado método calcula frequencia();

--- dadoWeb/src/br/com/k19/sesssionBeans/LancadorDeDado.java
+++ dadoWeb/src/br/com/k19/sesssionBeans/LancadorDeDado.java
@@ -1,5 +1,9 @@
 package br.com.k19.sesssionBeans;
 
+import java.util.Map;
+import java.util.concurrent.Future;
+
 public interface LancadorDeDado {
 	int lanca();
+	Future<Map<Integer, Integer>> calculaFrequencia();
 }
