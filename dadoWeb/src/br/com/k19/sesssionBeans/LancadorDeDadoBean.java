8be83545bb49cf1615d774a539cbed5682fad6df
Guilherme Alves Vargas <guilherme.alves.vargas@outlook.com>
2014-09-07 19:40:08 -0300

    Implementado metodo que calcula frequência.

--- dadoWeb/src/br/com/k19/sesssionBeans/LancadorDeDadoBean.java
+++ dadoWeb/src/br/com/k19/sesssionBeans/LancadorDeDadoBean.java
@@ -4,8 +4,14 @@ import java.util.Random;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.Future;
 
 @Stateless
 @Remote(LancadorDeDado.class)
@@ -13,6 +19,30 @@ public class LancadorDeDadoBean implements LancadorDeDado {
 	private Random gerador = new Random();
 	private static int contador;
 
+	@Asynchronous
+	public Future<Map<Integer, Integer>> calculaFrequencia() {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 0);
+		map.put(2, 0);
+		map.put(3, 0);
+		map.put(4, 0);
+		map.put(5, 0);
+		map.put(6, 0);
+		
+		for (int i = 0; i < 500; i++) {
+			int v = this.gerador.nextInt(6) + 1;
+			map.put(v, map.get(v) + 1);
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException ex) {}
+			
+			System.out.println(i);
+		}
+		
+		return new AsyncResult<Map<Integer,Integer>>(map);
+	}
+	
 	@PostConstruct
 	public void inicializando() {
 		synchronized (LancadorDeDadoBean.class) {
@@ -30,7 +60,7 @@ public class LancadorDeDadoBean implements LancadorDeDado {
 			System.out.println("Total: " + LancadorDeDadoBean.contador);
 		}
 	}
-
+	
 	public int lanca() {
 		return this.gerador.nextInt(6) + 1;
 	}
